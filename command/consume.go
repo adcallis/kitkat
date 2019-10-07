82985cf4e7363e1bb73d5a649a564a4342d39d08
Adam Callis <45042014+adcallis@users.noreply.github.com>
2019-10-06 23:48:23 -0400

    Update consume.go

--- command/consume.go
+++ command/consume.go
@@ -9,7 +9,7 @@ import (
 	"github.com/aws/aws-sdk-go/aws/credentials/processcreds"
 	"github.com/aws/aws-sdk-go/service/kinesis"
 	"github.com/spf13/cobra"
-	"github.com/waltzofpearls/kitkat/consumer"
+	"github.com/adcallis/kitkat/consumer"
 )
 
 func init() {
